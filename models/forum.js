abcc43d7d05a0aca10e672618e71e05d0bc5ea7a
namandangi <danginaman55@gmail.com>
2020-01-30 01:00:21 +0530

    restructure forum model

--- models/forum.js
+++ models/forum.js
@@ -2,48 +2,48 @@ const mongoose = require('mongoose');
 
 var forumSchema = new mongoose.Schema({
     question :
-    [{
+    {
         title : String ,
-        text : String ,
+        text : String ,        
+        createdAt :  { type : Date, default: Date.now },
+        updatedAt :  { type : Date, default: Date.now },
         poster :
         {
             type : mongoose.Schema.Types.ObjectId,
             ref : "User"
         },
-        createdAt :  { type : Date, default: Date.now },
-        updatedAt :  { type : Date, default: Date.now },
         upvoters :
-        {
+        [{
             type : mongoose.Schema.Types.ObjectId,
             ref : "User"
-        },
+        }],
         downvoters :
-        {
+        [{
             type : mongoose.Schema.Types.ObjectId,
             ref : "User"
-        },
     }],
+    },
 
     answers :
     [{
         text : String ,
+        createdAt :  { type : Date, default: Date.now },
+        updatedAt :  { type : Date, default: Date.now },
         poster :
         {
             type : mongoose.Schema.Types.ObjectId,
             ref : "User"
         },
-        createdAt :  { type : Date, default: Date.now },
-        updatedAt :  { type : Date, default: Date.now },
         upvoters :
-        {
+        [{
             type : mongoose.Schema.Types.ObjectId,
             ref : "User"
-        },
+        }],
         downvoters :
-        {
+        [{
             type : mongoose.Schema.Types.ObjectId,
             ref : "User"
-        },
+        }],
         pinned : {type:Boolean , default:false}
     }]
 });
