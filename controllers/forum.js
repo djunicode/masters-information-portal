95cfa1f55b0e44e673178be8ca5531a5c8d088b7
namandangi <danginaman55@gmail.com>
2020-01-30 22:26:46 +0530

    add CRUD for forums

--- controllers/forum.js
+++ controllers/forum.js
@@ -2,8 +2,8 @@ const   express     = require('express'),
         Forum       = require('../models/forum'),
         router      = express.Router();
 
-
-        router.get('/',async (request,response)=>{            
+        //FIND ALL
+        router.get('/', async (request,response)=>{            
             await Forum.find({},(error,allForums)=>{
                 if(error)
                 {
@@ -17,37 +17,13 @@ const   express     = require('express'),
             });
         });
 
-
-        router.post('/',(request,response)=>{
-            //console.log(request.body);
-            //var body = JSON.stringify(request.body);
-            var newResponse = {
-                 question : {
-                 title :  request.body.question.title,
-                 text : request.body.question.text,
-                 createdAt : request.body.question.createdAt,
-                 updatedAt : request.body.question.createdAt,
-                 //poster : request.body.question.poster,
-                 upvoters : request.body.question.upvoters,
-                 downvoters : request.body.question.downvoters
-                 },
-                answer : [{
-                 text : request.body.answers[0].text,
-                 createdAt : request.body.answers[0].createdAt,
-                 updatedAt : request.body.answers[0].createdAt,
-                 //poster : request.body.answers[0].poster,
-                 upvoters : request.body.answers[0].upvoters,
-                 downvoters : request.body.answers[0].downvoters,
-                 pinned : request.body.answers[0].pinned                 
-                }]                
-            }
-            console.log(newResponse);
-            console.log("gonna start storing into db");
-             Forum.create(newResponse,(error,newForum)=>{
+        //CREATE 
+        router.post('/', async (request,response)=>{            
+             await Forum.create(request.body,(error,newForum)=>{
                 if(error)
                 {
                     console.log(error);
-                    response.redirect('/');
+                    response.redirect('/api/forum');
                 }
                 else if(!error)
                 {
@@ -55,7 +31,47 @@ const   express     = require('express'),
                     response.send(newForum);
                 }
             })
-            console.log("finished storing in the db");
         });
+        //FIND BY ID
+        router.get("/:id",async function(request, response){            
+            await Forum.findById(request.params.id,(error, foundForum)=>{
+                if(error)
+                {
+                    console.log(err);
+                } else 
+                {                    
+                    response.send(foundForum);
+                }
+            });
+        });
+
+
+        //UPDATE
+        router.put("/:id",async (request,response)=>{
+            await Forum.findByIdAndUpdate(request.params.id,request.body,(error,forum)=>{
+                if(error)
+                {
+                    console.log(err);
+                    response.redirect("/api/forum");
+                }
+                else
+                {
+                    response.send(forum);
+                }
+            });    
+        });
+
+        //DESTROY
+        router.delete("/:id",(request,response)=>{
+        Forum.findByIdAndDelete(request.params.id,(error)=>{
+            if(error)
+            {
+                console.log(error);
+            }
+            else
+            response.redirect("/api/forum");
+        });
+     });
+
 
         module.exports = router;
