6228e630b4e67b7218a2c362eaf0640ae108d731
namandangi <danginaman55@gmail.com>
2020-03-03 00:42:09 +0530

    major variable name revamp

--- controllers/chat.js
+++ controllers/chat.js
@@ -2,48 +2,56 @@ const express = require('express');
 const mongo = require('../models/chat');
 const router = express.Router();
 
-let name;
-let toWhom;
-let inid;
-let toid;
+let senderId;
+let receiverId;
+//why are we using name,senderId,recevierId,TWhom when we arent even storing it ??
+//if we dont need them we can send them to get req block to avoid deadlock
 
 router.post("/chat", async(req, res) => {
+
+let name;
+let TWhom;
     // Finding from db if the user is present 
-    const init=await mongo.User.findOne({name:req.body.initiator});
-    const to=await mongo.User.findOne({name:req.body.to});
-    const ini=await mongo.Message.findOne({initiator:init._id,to:to._id});
-    const ini2=await mongo.Message.findOne({initiator:to._id,to:init._id});
+    const sender = await mongo.User.findOne({name:req.body.sender});
+    const receiver = await mongo.User.findOne({name:req.body.receiver});
+    const messageSender = await mongo.Message.findOne({sender:sender._id,receiver:receiver._id});
+    const messageReceiver = await mongo.Message.findOne({sender:receiver._id,receiver:sender._id});
+    //here messageReceiver is actually a sender
     
-    if(ini){
-        name=req.body.initiator;
-        toWhom=req.body.to;
-        res.send(ini.message)
-        inid=init._id;
-        toid=to._id
+    if(messageSender)
+    {
+        
+        name = req.body.sender;
+        TWhom = req.body.receiver;
+        res.send(messageSender.message)
+        senderId = sender._id;
+        receiverId = receiver._id
     }
-    else if(ini2){
-        name=req.body.to;
-        toWhom=req.body.initiator;
-        res.send(ini2.message)
-        inid=to._id;
-        toid=init._id
+    else if(messageReceiver)
+    {
+        name = req.body.receiver;
+        TWhom = req.body.sender;
+        res.send(messageReceiver.message)
+        senderId = receiver._id;
+        receiverId = sender._id
     }
-    else{
-        const newUser=new mongo.Message({
-            initiator:await mongo.User.findOne({name:req.body.initiator}),
-            to:await mongo.User.findOne({name:req.body.to})
+    else
+    {
+        const newUser = new mongo.Message({
+            sender:await mongo.User.findOne({name:req.body.sender}),
+            receiver:await mongo.User.findOne({name:req.body.receiver})
         }).save().then(data=>{
             console.log(data);
             res.send(data);
         }).catch(e=>console.log(e))
-        name=req.body.initiator;
-        toWhom=req.body.to; 
+        name = req.body.sender;
+        TWhom = req.body.receiver; 
     }
 });
 
 // To display the previous chats 
 router.get("/ret", async(req, res) => {
-    const data=await mongo.Message.findOne({initiator:inid,to:toid});
+    const data = await mongo.Message.findOne({sender:senderId,receiver:receiverId});
     console.log(data.message)
     res.send(data.message)
 })
