9d1d0f2dfc2a00bd199655c8d9bf3328e6831d21
Yashodhan Joshi <yash.sumedha@gmail.com>
2020-03-03 20:42:09 +0530

    Patch  index.js for testing after merging changes by Jay Mehta (@jaypajji4444)

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8002;
 const HOST = process.env.HOST || 'localhost';
-const logger=require("./config/logger")
+const logger = require('./config/logger');
 
-app.listen(PORT, HOST, () => {
+module.exports = app.listen(PORT, HOST, () => {
   logger.info(`Server started listening on http://${HOST}:${PORT}`);
   console.info(`Server started listening on http://${HOST}:${PORT}`);
 });
