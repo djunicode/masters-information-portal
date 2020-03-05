6410678f6b981f28c607f9338dcb1e406fe61e92
Yash! <chachadyash786@gmail.com>
2020-03-05 19:44:08 +0530

    Created middleware folder and added auth middleware

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { encryptPassword, comparePassword } = require('../infra/encryption');
-const { auth } = require("../infra/jwt/index")
+const { auth } = require("../infra/middleware/auth")
 const User = require("../models/user")
 const router = express.Router();
 const bcrypt = require("bcryptjs")
