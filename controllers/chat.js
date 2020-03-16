3766d6844053aed5a6d188b455a2e8e2ca0059a7
unknown <hethgala@gmail.com>
2020-03-16 16:59:09 +0530

    Socketio updates

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,15 +1,10 @@
 const Chat = require('../models/chat');
 const logger = require('../config/logger');
-const auth=require('../middleware/auth');
-const express=require('express');
 
-const router = express.Router();
 /**
  * @route POST "/api/chat"
  */
-router.post('/',auth,create);
-
-exports.create = async (req, res) => {
+ exports.create = async (req, res) => {
   const doc = await Chat.create(req.body);
   logger.created('Chat', doc);
   return res.status(201).json(doc);
--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const asyncHandler = require('express-async-handler');
 const controller = require('../controllers/chat');
+const auth=require('../middleware/auth')
 
 const router = Router();
-router.post('/', asyncHandler(controller.create));
+router.post('/',auth,asyncHandler(controller.create));
 
 module.exports = router;
