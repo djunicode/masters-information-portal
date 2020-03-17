37d6ae38d28e073ee5815ecc664e3377a790700d
Nimit98 <54390245+Nimit98@users.noreply.github.com>
2020-03-17 21:34:17 +0530

    chat updated

--- webapp/src/Components/router.js
+++ webapp/src/Components/router.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import Login from './login';
-import Forum from './forum';
-import NavBar from './navbar';
-import Messenger from './MainMessenger/Messenger/index'
-import List from './MainMessenger/List/List'
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Login from "./login";
+import Forum from "./forum";
+import NavBar from "./navbar";
+import Messages from "./Messages";
 class RootRouter extends React.Component {
-    state = {
-        user: null,
-        isLoggedIn: false
-    };
+  state = {
+    user: null,
+    isLoggedIn: false
+  };
 
-    render() {
-        return (
-            <Router>
-                <NavBar />
-                <Switch>
-                    <Route exact path='/' />
-                    <Route exact path='/login' component={Login} />
-                    <Route exact path='/profile' />
-                    <Route exact path='/forum' component={Forum} />
-                    <Route exact path='/chat' component={List} />
-                    <Route exact path='/chat/1' component={Messenger} />
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" />
+          <Route exact path="/forum" component={Forum} />
+          <Route exact path="/chat" component={Messages} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default RootRouter;
