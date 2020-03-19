586c4acf728581b9deb9a0d7b3089a85dc834fd9
YatharthVyas <yatharth2000.vyas@gmail.com>
2020-03-19 13:32:36 +0530

    fix conflict

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,23 +7,23 @@ const { verifyJwt } = require('../infra/jwt');
  * Authorization: Bearer token
  */
 const authRequired = async (req, res, next) => {
-  const header = req.header('Authorization');
-  if (!header) {
-    return res.status(400).send({ msg: 'Please Provide JWT' });
-  }
-  const token = header.replace('Bearer', '').trim();
-  const decoded = verifyJwt(token);
-  const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-
-  if (!user) {
+  const token = req.header('Authorization').replace('Bearer', '').trim();
+  try{
+    const decoded = await verifyJwt(token);
+    if(!decoded){
+      return res.status(401).json({
+        msg: 'Invalid token',
+      });
+    }
+    const user = await User.findOne({ _id: decoded._id });
+    req.token = token;
+    req.user = user;
+    next();
+  }catch(e){
     return res.status(401).json({
       msg: 'Invalid token'
     });
   }
-
-  req.token = token;
-  req.user = user;
-  next();
 };
 
 module.exports = { authRequired };
