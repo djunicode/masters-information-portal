1b56585e2642c76f3563caf52cdfb7e944f690a9
namandangi <danginaman55@gmail.com>
2020-03-19 17:07:47 +0530

    fix forum route

--- routes/forum.js
+++ routes/forum.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const {authRequired} = require('../middleware/auth');
-const {owner} = require('../middleware/forumAuth');
+const {isOwner} = require('../middleware/forumAuth');
 const asyncHandler = require('express-async-handler');
 const controller = require('../controllers/forum');
 
@@ -8,7 +8,7 @@ const router = Router();
 router.post('/',authRequired, asyncHandler(controller.create));
 router.get('/', asyncHandler(controller.getAll));
 router.get('/:id', asyncHandler(controller.getById));
-router.put('/:id',authRequired,owner, asyncHandler(controller.updateById));
-router.delete('/:id',authRequired,owner, asyncHandler(controller.deleteById));
+router.put('/:id',authRequired,isOwner, asyncHandler(controller.updateById));
+router.delete('/:id',authRequired,isOwner, asyncHandler(controller.deleteById));
 
 module.exports = router;
