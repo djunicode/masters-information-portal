72dc692a519aac27d3d8daf059906b1929cd972e
unknown <hethgala@gmail.com>
2020-03-26 22:59:02 +0530

    Readability enhancing code

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,15 @@ const asyncHandler = require('express-async-handler');
 const { authRequired,hasRoles} = require('../middleware/auth');
 const { upload } = require('../middleware/multer');
 const controller = require('../controllers/user');
+const reqdRoles=require('../constants/roles')
 
 const router = Router();
+
 router.post('/register', asyncHandler(controller.register));
 router.post('/login', asyncHandler(controller.login));
 router.post('/refresh',asyncHandler(controller.refresh))
 router.post('/upload',authRequired,upload.single('avatar'),asyncHandler(controller.uploadProfilePhoto));
-router.get('/me', authRequired,hasRoles(['Admin']),asyncHandler(controller.getProfile));
+router.get('/me', authRequired,hasRoles(reqdRoles),asyncHandler(controller.getProfile));
 router.get('/:id', asyncHandler(controller.getById));
 router.get('/:id/avatar', asyncHandler(controller.getProfilePhoto));
 router.put('/me', authRequired, asyncHandler(controller.updateProfile));
