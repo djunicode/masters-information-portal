31ba43968800758621d19c65e8f2b23082b23c04
Yashodhan Joshi <yash.sumedha@gmail.com>
2020-05-03 18:05:07 +0530

    Updated test configuration

--- config/constants.js
+++ config/constants.js
@@ -2,23 +2,23 @@ const path = require('path');
 const dotEnv = require('dotenv');
 
 const parsedEnv = dotEnv.config({
-	path: path.join(__dirname, '../.env.development'),
+  path: path.join(__dirname, '../.env.development'),
 });
 
 if (parsedEnv.error) {
-	throw parsedEnv.error;
+  throw parsedEnv.error;
 }
 
 const config = {
-	port: Number(process.env.PORT) || 3000,
-	host: process.env.HOST || 'localhost',
-	jwtSecret: process.env.JWT_SECRET,
-	jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-	saltRounds: Number(process.env.SALT_ROUNDS),
-	rateLimitWindowInterval: Number(process.env.RATE_LIMIT_WINDOW_INTERVAL),
-	rateLimitAttempts: Number(process.env.RATE_LIMIT_ATTEMPTS),
-	dbConnectionUrl: process.env.DB_CONNECTION_URL,
-	dbTestConnectionUrl: process.env.DB_TEST_CONNECTION_URL,
+  port: Number(process.env.PORT) || 3000,
+  host: process.env.HOST || 'localhost',
+  jwtSecret: process.env.JWT_SECRET,
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+  saltRounds: Number(process.env.SALT_ROUNDS),
+  rateLimitWindowInterval: Number(process.env.RATE_LIMIT_WINDOW_INTERVAL),
+  rateLimitAttempts: Number(process.env.RATE_LIMIT_ATTEMPTS),
+  dbConnectionUrl: process.env.DB_CONNECTION_URL,
+  dbTestConnectionUrl: process.env.DB_TEST_CONNECTION_URL,
 };
 
 module.exports = config;
