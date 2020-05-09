0927b7b2a0e056f8d0a94d9ec0981e0b14d722d8
Nimit98 <54390245+Nimit98@users.noreply.github.com>
2020-05-09 14:37:57 +0530

    Added QuestionReplyPage

--- webapp/src/Components/Router/router.js
+++ webapp/src/Components/Router/router.js
@@ -7,30 +7,33 @@ import EditProfile from '../../Pages/Edit Profile/editProfile';
 import Resources from '../resources';
 import Messages from '../Messages';
 import AddResource from '../AddResources';
-import FullWidthTabs from '../profpage3'
-function RootRouter(props){
+import FullWidthTabs from '../profpage3';
+import Question_Detail from '../Questions_Detail';
 
-    const renderLogin = () => <Login loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn}/>;
-	const renderChat = () => <Messages loggedIn={props.loggedIn} />;
-	const renderAddResource = () => <AddResource loggedIn={props.loggedIn} />;
-	const invalidRoute = () => <Redirect to='/'/>;
+function RootRouter(props) {
+  const renderLogin = () => <Login loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn} />;
+  const renderChat = () => <Messages loggedIn={props.loggedIn} />;
+  const renderAddResource = () => <AddResource loggedIn={props.loggedIn} />;
+  const questionDetail = () => <Question_Detail loggedIn={props.loggedIn} />;
+  const invalidRoute = () => <Redirect to="/" />;
 
-    return (
-        <Router>
-            <NavBar loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn}/>
-            <Switch>
-                <Route exact path='/' />
-                <Route exact path='/login' component={renderLogin}/>
-                <Route exact path='/profile' component={FullWidthTabs}/>
-                <Route exact path='/edit' component={EditProfile}/>
-                <Route exact path='/resources' component={Resources} />
-                <Route exact path='/forum' component={Forum} />
-				<Route exact path="/chat" component={renderChat} />
-				<Route exact path="/add-resource" component={renderAddResource} />
-				<Route component = {invalidRoute} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <NavBar loggedIn={props.loggedIn} setLoggedIn={props.setLoggedIn} />
+      <Switch>
+        <Route exact path="/" />
+        <Route exact path="/login" component={renderLogin} />
+        <Route exact path="/profile" component={FullWidthTabs} />
+        <Route exact path="/edit" component={EditProfile} />
+        <Route exact path="/resources" component={Resources} />
+        <Route exact path="/forum" component={Forum} />
+        <Route exact path="/chat" component={renderChat} />
+        <Route exact path="/add-resource" component={renderAddResource} />
+        <Route exact path="/question-detail" component={questionDetail} />
+        <Route component={invalidRoute} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default RootRouter;
